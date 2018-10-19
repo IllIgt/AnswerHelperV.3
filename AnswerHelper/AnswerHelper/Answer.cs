1a2688ab70a2f2b3ca4c57e49454efe2bc754c37
Alexey Ratnikov <Voblin_ne@mail.ru>
2018-10-19 18:22:11 +0300

    Initial commit
    Answer and rearragment classes added

---
+++ AnswerHelper/AnswerHelper/Answer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnswerHelper
+{
+    class Answer
+    {
+        public List<Rearrangement> Rearrangements { get; set; }
+        private string _Date;
+        private static Answer Instance;
+
+        public Answer()
+        {
+            _Date = DateTime.Now.ToString("dd MMMM yyyy");
+        }
+
+        public static Answer GetInstance()
+        {
+            if (Instance == null)
+                Instance = new Answer();
+            return Instance;
+        }
+        
+    }
+}
---
+++ AnswerHelper/AnswerHelper/Rearrangement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnswerHelper
+{
+    class Rearrangement
+    {
+        private int _Size;
+        public string Type { get; set; }
+        public int StartLocation { get; set; }
+        public int EndLocation { get; set; }
+        public string ChromosomeLocus { get; set; }
+
+        public Rearrangement(string chromosome_locus, int start_location, int end_location, string type)
+        {
+            StartLocation = start_location;
+            EndLocation = end_location;
+            _Size = end_location - start_location;
+            Type = type;
+            ChromosomeLocus = chromosome_locus;
+        }
+
+        public override string ToString()
+        {
+            return $"\b{Type}\b0 {ChromosomeLocus} (геномная локализация: {StartLocation}-{EndLocation};" +
+                    $" размер: {_Size} пн): {Type}, затронувшая";
+        }
+
+    }
+}
