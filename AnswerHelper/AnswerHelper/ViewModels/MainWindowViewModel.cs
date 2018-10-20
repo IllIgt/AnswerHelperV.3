c103f597a2867fafc5359b62c2f85ba8fedb2694
IllIgt <Voblin_ne@mail.ru>
2018-10-20 19:40:11 +0300

    ViewModels Added

---
+++ AnswerHelper/AnswerHelper/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AnswerHelper
+{
+    class MainWindowViewModel : ViewModel
+    {
+        public ICommand AddCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new Command(OnAddCommandExecuted);
+        }
+
+        private void OnAddCommandExecuted(object obj)
+        {
+            RearrangementWindow rearrangementWindow = new RearrangementWindow();
+            rearrangementWindow.ShowDialog();
+        }
+    }
+}
